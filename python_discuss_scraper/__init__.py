bb6157b429a6fc402d433678355dd7d3d87e3272
Erik Dreyer <erik.dreyer@protonmail.com>
2023-07-30 22:51:32 +0200

    scraper init

---
+++ python_discuss_scraper/__init__.py
@@ -0,0 +1,22 @@
+import logging
+
+__all__ = ["mastoBot", "configManager"]
+
+# Logging setup
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)-25s - [%(levelname)s] - %(module)s.%(funcName)-30s - %(message)-50s",
+)
+
+# File logger
+file_logger = logging.FileHandler("scraper.log", "w")
+file_logger.setLevel(logging.INFO)
+file_logger.setFormatter(
+    logging.Formatter(
+        "%(asctime)-25s - [%(levelname)s] - %(module)s.%(funcName)-30s - %(message)-50s"
+    )
+)
+
+# Get the root logger
+root_logger = logging.getLogger()
+root_logger.addHandler(file_logger)
---
+++ python_discuss_scraper/python_discuss_scraper.py
@@ -0,0 +1,5 @@
+
+class PythonDiscussScraper:
+    
+    def __init__(self) -> None:
+        pass
